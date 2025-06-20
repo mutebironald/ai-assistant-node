5c0c4589279d5b103513d323e401e69e7232bcec
Ronald Mutebi <mutebironaldroninho@gmail.com>
2025-06-20 11:33:17 +0300

    add openai logic

---
+++ openai.js
@@ -0,0 +1,23 @@
+// const { Configuration, OpenAIApi } = require("openai");
+const OpenAI = require("openai");
+require("dotenv").config();
+
+// const configuration = new Configuration({
+//   apiKey: process.env.OPENAI_API_KEY,
+// });
+
+// const openai = new OpenAIApi(configuration);
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+async function askAI(prompt) {
+  const res = await openai.chat.completions.create({
+    model: process.env.OPENAI_MODEL, // or "gpt-3.5-turbo"
+    messages: [{ role: "user", content: prompt }],
+  });
+
+  return res.choices[0].message.content.trim();
+}
+
+module.exports = { askAI };
